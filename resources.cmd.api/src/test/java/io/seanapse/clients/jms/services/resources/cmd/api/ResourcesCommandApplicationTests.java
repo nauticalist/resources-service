afbd7e26cd3f368aea95b6586ae61ae116035608
Ismail Aksoy <iaksoy@seanapse.io>
2020-12-22 15:05:49 +0300

    fixed config parameters

--- resources.cmd.api/src/test/java/io/seanapse/clients/jms/services/resources/cmd/api/ResourcesCommandApplicationTests.java
+++ resources.cmd.api/src/test/java/io/seanapse/clients/jms/services/resources/cmd/api/ResourcesCommandApplicationTests.java
@@ -1,9 +1,11 @@
 package io.seanapse.clients.jms.services.resources.cmd.api;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.data.mongo.AutoConfigureDataMongo;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureDataMongo
 class ResourcesCommandApplicationTests {
 
 	@Test
